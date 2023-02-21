4c98065f72387ae1628d4cb99ba83b7e0f4d1e81
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2023-02-21 12:36:07 +1100

    display all restaurants and add review

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_000039) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_002214) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom',
+            address: '7 Boundary St, London E2 7JE',
+            phone_number: 416_521_650,
+            category: 'chinese' }
+
+pizza_east = { name: 'Pizza East',
+               address: '56A Shoreditch High St, London E1 6PQ',
+               phone_number: 0416_421_650,
+               category: 'italian' }
+
+turkish_grill = { name: 'Golden Grill',
+                  address: '7 Watton St, Werribee, Melbourne',
+                  phone_number: 0416_521_650,
+                  category: 'japanese' }
+
+indo_eats = { name: 'IndoEats',
+              address: '15 Synnot St, London E2 7JE',
+              phone_number: 0416_221_650,
+              category: 'belgian' }
+
+lola_cocina = { name: 'Lola Cocina',
+                address: '9 Boundary St, Sydney E2 7JE',
+                phone_number: 0416_521_650,
+                category: 'french' }
+
+[dishoom, pizza_east, turkish_grill, indo_eats, lola_cocina].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
